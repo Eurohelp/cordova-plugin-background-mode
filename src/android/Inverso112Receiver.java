29235b34bae66b215d27757612ddc8ca50629f2f
Asier Fernández de Aguirre <30799963+asierf@users.noreply.github.com>
2020-01-20 09:58:13 +0100

    Update Inverso112Receiver.java

--- src/android/Inverso112Receiver.java
+++ src/android/Inverso112Receiver.java
@@ -2,6 +2,7 @@ package de.appplant.cordova.plugin.background;
 
 import android.content.Intent;
 import android.content.BroadcastReceiver;
+import android.os.Build;
 import android.util.Log;
 import android.content.Context;
 
@@ -12,8 +13,13 @@ public class Inverso112Receiver extends BroadcastReceiver {
   public void onReceive(Context context, Intent intent) {
     try {
       Log.d(TAG, "Starting on boot");
-      Intent serviceIntent = new Intent(context, Inverso112Service.class);
-      context.startService(serviceIntent);
+      /*Intent serviceIntent = new Intent(context, Inverso112Service.class);
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        context.startForegroundService(serviceIntent);
+      } else {
+        context.startService(serviceIntent);
+      }*/
+      Inverso112Service.schedulerJob(context);
     } catch(Exception ex) {
       Log.d(TAG, "Cannot start app on boot. Exception" + ex.toString());
     }

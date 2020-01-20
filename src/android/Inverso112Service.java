066d66558c7bb55cd92c1e3cca929b75d572c42f
Asier Fernández de Aguirre <30799963+asierf@users.noreply.github.com>
2020-01-20 09:57:15 +0100

    Update Inverso112Service.java

--- src/android/Inverso112Service.java
+++ src/android/Inverso112Service.java
@@ -1,59 +1,49 @@
 package de.appplant.cordova.plugin.background;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.Handler;
-import android.os.IBinder;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
 import android.util.Log;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 
-public class Inverso112Service extends Service {
 
-    private static final String TAG = "Inverso112Service";
+public class Inverso112Service extends JobService {
 
-    public static final int interval = 300000;  //interval between two services(Here Service run every 5 Minute)
+  private static final String TAG = "Inverso112Service";
 
-    private Handler handler = new Handler();   //run on another Thread to avoid crash
-    private Timer timer = null;    //timer handling
+  public static void schedulerJob(Context context) {
+      ComponentName serviceComponent = new ComponentName(context, Inverso112Service.class);
+      JobInfo.Builder builder = new JobInfo.Builder(0,serviceComponent);
+      builder.setMinimumLatency(30*1000);    // wait at least
+      builder.setOverrideDeadline(60*1000);  //delay time
+      builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED);  // require unmetered network
+      builder.setRequiresCharging(false);  // we don't care if the device is charging or not
+      builder.setRequiresDeviceIdle(true); // device should be idle
+        Log.d(TAG, "scheduler Job");
+
+      JobScheduler jobScheduler = null;
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+        jobScheduler = context.getSystemService(JobScheduler.class);
+      }
+      jobScheduler.schedule(builder.build());
+  }
+
 
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public boolean onStartJob(JobParameters params) {
+      Inverso112Service.schedulerJob(getApplicationContext()); // reschedule the job
+      Log.d(TAG, "onStartJob");
+      return true;
     }
 
     @Override
-    public void onCreate() {
-        if (timer != null) { // Cancel if already existed
-            timer.cancel();
-        } else {
-            timer = new Timer();   //recreate new
-        }
-        timer.scheduleAtFixedRate(new TimeDisplay(), 0, interval);   //Schedule task
-        Log.d(TAG, "onCreate");
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        timer.cancel();
-        Log.d(TAG, "onDestroy");
-    }
-
-    class TimeDisplay extends TimerTask {
-
-        @Override
-        public void run() {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    Log.d(TAG, "TimeDisplay");
-                }
-            });
-        }
-
+    public boolean onStopJob(JobParameters params) {
+      Log.d(TAG, "onCreate");
+      return true;
     }
 
 }

7e8a58344d811df87ef56d0a4c3aeb8bbcfe5838
Asier Fernández de Aguirre <30799963+asierf@users.noreply.github.com>
2020-01-21 12:40:55 +0100

    Update Inverso112Service.java

--- src/android/Inverso112Service.java
+++ src/android/Inverso112Service.java
@@ -1,65 +1,270 @@
 package de.appplant.cordova.plugin.background;
 
-import android.app.job.JobParameters;
-import android.app.job.JobService;
+import android.annotation.TargetApi;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Build;
+import android.os.IBinder;
 import android.util.Log;
 
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 
 import android.Manifest;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 
-public class Inverso112Service extends JobService {
+import org.json.JSONObject;
+
+public class Inverso112Service extends Service {
 
   private static final String TAG = "Inverso112Service";
 
-  public static void schedulerJob(Context context) {
-      ComponentName serviceComponent = new ComponentName(context, Inverso112Service.class);
-      JobInfo.Builder builder = new JobInfo.Builder(0, serviceComponent);
-      builder.setMinimumLatency(1 * 1000 * 60);    // wait at least
-      builder.setOverrideDeadline(2 * 1000 * 60);  //delay time
-      builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED);  // require unmetered network
-      builder.setRequiresCharging(false);  // we don't care if the device is charging or not
-      builder.setRequiresDeviceIdle(true); // device should be idle
-      Log.d(TAG, "schedulerJob");
 
-      JobScheduler jobScheduler = null;
-      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-        jobScheduler = context.getSystemService(JobScheduler.class);
-        jobScheduler.schedule(builder.build());
-      } else{
-        Log.d(TAG, "NO schedulerJob");
-      }
+  // Fixed ID for the 'foreground' notification
+  public static final int NOTIFICATION_ID = -574595411;
+
+  // Default title of the background notification
+  private static final String NOTIFICATION_TITLE =
+    "112 Inverso is running in background";
+
+  // Default text of the background notification
+  private static final String NOTIFICATION_TEXT =
+    "Doing heavy tasks.";
+
+  // Default icon of the background notification
+  private static final String NOTIFICATION_ICON = "icon";
+
+  // Binder given to clients
+  private final IBinder binder = new Inverso112Service.Inverso112Binder();
+
+  /**
+   * Allow clients to call on to the service.
+   */
+  @Override
+  public IBinder onBind (Intent intent) {
+    return binder;
+  }
+
+  /**
+   * Class used for the client Binder.  Because we know this service always
+   * runs in the same process as its clients, we don't need to deal with IPC.
+   */
+  class Inverso112Binder extends Binder
+  {
+    Inverso112Service getService()
+    {
+      return Inverso112Service.this;
+    }
+  }
+
+  /**
+   * Put the service in a foreground state to prevent app from being killed
+   * by the OS.
+   */
+  @Override
+  public void onCreate()
+  {
+    super.onCreate();
+    startForeground(NOTIFICATION_ID, makeNotification());
+
+    setAlarm();
+    if(hasPermission()) {
+      Log.d(TAG, "hasPermission() = true");
+      getLocation();
+    } else {
+      Log.d(TAG, "hasPermission() = false");
+      stopSelf();
+    }
+
   }
 
 
+  /**
+   * No need to run headless on destroy.
+   */
+  @Override
+  public void onDestroy()
+  {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy");
+    sleepWell();
+  }
 
-    @Override
-    public boolean onStartJob(JobParameters params) {
-      Inverso112Service.schedulerJob(getApplicationContext()); // re-schedule the job
-      Log.d(TAG, "onStartJob");
 
-      if(hasPermission()) {
-        Log.d(TAG, "hasPermission() = true");
-        getLocation();
-      } else {
-        Log.d(TAG, "hasPermission() = false");
-      }
+  /**
+   * Stop background mode.
+   */
+  private void sleepWell()
+  {
+    stopForeground(true);
+    getNotificationManager().cancel(NOTIFICATION_ID);
+  }
 
-      return true;
+  private void setAlarm(){
+    AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+    Intent alarmIntent = new Intent(this, Inverso112Receiver.class);
+    alarmIntent.setAction(Inverso112Receiver.ACTION_ALARM);
+    PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+    alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (1000 * 60 * 30), pendingIntent);
+  }
+
+     /* Create a notification as the visible part to be able to put the service
+     * in a foreground state by using the default settings.
+     */
+  private Notification makeNotification()
+  {
+    return makeNotification(BackgroundMode.getSettings());
+  }
+
+  /**
+   * Create a notification as the visible part to be able to put the service
+   * in a foreground state.
+   *
+   * @param settings The config settings
+   */
+  private Notification makeNotification (JSONObject settings)
+  {
+    // use channelid for Oreo and higher
+    String CHANNEL_ID = "cordova-plugin-background-mode-112-inverso";
+    if(Build.VERSION.SDK_INT >= 26){
+      // The user-visible name of the channel.
+      CharSequence name = "cordova-plugin-background-mode-112-inverso";
+      // The user-visible description of the channel.
+      String description = "cordova-plugin-background-moden-112-inverso notification";
+
+      int importance = NotificationManager.IMPORTANCE_LOW;
+
+      NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+
+      // Configure the notification channel.
+      mChannel.setDescription(description);
+
+      getNotificationManager().createNotificationChannel(mChannel);
+    }
+    String title    = settings.optString("title", NOTIFICATION_TITLE);
+    String text     = settings.optString("text", NOTIFICATION_TEXT);
+    boolean bigText = settings.optBoolean("bigText", false);
+
+    Context context = getApplicationContext();
+    String pkgName  = context.getPackageName();
+    Intent intent   = context.getPackageManager()
+      .getLaunchIntentForPackage(pkgName);
+
+    Notification.Builder notification = new Notification.Builder(context)
+      .setContentTitle(title)
+      .setContentText(text)
+      .setOngoing(true)
+      .setSmallIcon(getIconResId(settings));
+
+    if(Build.VERSION.SDK_INT >= 26){
+      notification.setChannelId(CHANNEL_ID);
     }
 
-    @Override
-    public boolean onStopJob(JobParameters params) {
-      Log.d(TAG, "onStopJob");
-      return true;
+    if (settings.optBoolean("hidden", true)) {
+      notification.setPriority(Notification.PRIORITY_MIN);
     }
+
+    if (bigText || text.contains("\n")) {
+      notification.setStyle(
+        new Notification.BigTextStyle().bigText(text));
+    }
+
+    setColor(notification, settings);
+
+    if (intent != null && settings.optBoolean("resume")) {
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+      PendingIntent contentIntent = PendingIntent.getActivity(
+        context, NOTIFICATION_ID, intent,
+        PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+      notification.setContentIntent(contentIntent);
+    }
+
+    return notification.build();
+  }
+
+  /**
+   * Retrieves the resource ID of the app icon.
+   *
+   * @param settings A JSON dict containing the icon name.
+   */
+  private int getIconResId (JSONObject settings)
+  {
+    String icon = settings.optString("icon", NOTIFICATION_ICON);
+
+    int resId = getIconResId(icon, "mipmap");
+
+    if (resId == 0) {
+      resId = getIconResId(icon, "drawable");
+    }
+
+    return resId;
+  }
+
+  /**
+   * Retrieve resource id of the specified icon.
+   *
+   * @param icon The name of the icon.
+   * @param type The resource type where to look for.
+   *
+   * @return The resource id or 0 if not found.
+   */
+  private int getIconResId (String icon, String type)
+  {
+    Resources res  = getResources();
+    String pkgName = getPackageName();
+
+    int resId = res.getIdentifier(icon, type, pkgName);
+
+    if (resId == 0) {
+      resId = res.getIdentifier("icon", type, pkgName);
+    }
+
+    return resId;
+  }
+
+  /**
+   * Set notification color if its supported by the SDK.
+   *
+   * @param notification A Notification.Builder instance
+   * @param settings A JSON dict containing the color definition (red: FF0000)
+   */
+  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+  private void setColor (Notification.Builder notification, JSONObject settings)
+  {
+
+    String hex = settings.optString("color", null);
+
+    if (Build.VERSION.SDK_INT < 21 || hex == null)
+      return;
+
+    try {
+      int aRGB = Integer.parseInt(hex, 16) + 0xFF000000;
+      notification.setColor(aRGB);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Returns the shared notification service manager.
+   */
+  private NotificationManager getNotificationManager()
+  {
+    return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+  }
+
 
     private void getLocation() {
       Log.d(TAG, "getLocation");
@@ -72,27 +277,28 @@ public class Inverso112Service extends JobService {
         Log.d(TAG, "isGPSEnabled = " + isGPSEnabled);
         Log.d(TAG, "isNetworkEnabled = " + isNetworkEnabled);
 
-        /*if (!isNetworkEnabled && !isGPSEnabled) {
-          // cannot get location
+        // com.google.android.gms.common.api.GoogleApi.FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (!isNetworkEnabled && !isGPSEnabled) {
+          sendLocation(getLastKnownLocation(locationManager));
         } else {
           Location location = null;
-          if (isNetworkEnabled) {
-            // locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_DISTANCE_CHANGE_FOR_UPDATES, MIN_TIME_BW_UPDATES, locationListener);
-            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-          } else if (isGPSEnabled)  {
+          if (isGPSEnabled)  {
             // locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_DISTANCE_CHANGE_FOR_UPDATES, MIN_TIME_BW_UPDATES, locationListener);
             location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+          } else if (isNetworkEnabled) {
+            // locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_DISTANCE_CHANGE_FOR_UPDATES, MIN_TIME_BW_UPDATES, locationListener);
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
           }
           sendLocation(location);
-        }*/
-        sendLocation(getLastKnownLocation(locationManager));
+        }
       } catch ( SecurityException e ) {
         Log.d(TAG, "getLocation error", e.getCause());
+        stopSelf();
       }
     }
     private void sendLocation(Location location) {
       if(location != null) {
-
           Thread thread = new Thread(() -> {
               try {
                 java.net.URL url = new java.net.URL("http://docker.eurohelp.es:5555/api/v1/tracking");
@@ -136,11 +342,13 @@ public class Inverso112Service extends JobService {
               } catch (Exception e) {
                 Log.d(TAG , "ERROR = " + e.getMessage());
               }
+              stopSelf();
           });
 
           thread.start();
       } else {
         Log.d(TAG, "location = null");
+        stopSelf();
       }
     }
 
